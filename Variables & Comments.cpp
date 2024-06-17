55e83c53ba93e45f5cedb8130764b057d28b83b4
VanshDivyanshu <109081574+VanshDivyanshu@users.noreply.github.com>
2024-06-18 01:35:48 +0530

    Add files via upload

---
+++ Variables & Comments.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// This is a COMMENT !!
+
+/*This 
+is 
+a 
+multi - line 
+comment*/
+
+int main()
+{   
+    int a = 16;
+    string name = "Vansh";
+    cout << "Hello World"<<","<<" today's date is "<<a<<"th of January"<<endl;
+    cout<<"Happy Birthday to "<<name;
+    return 0;
+}
