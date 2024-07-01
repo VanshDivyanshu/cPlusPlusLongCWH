8ca7c12f7c230cd560f51c1c15ab213bd4c58e32
VanshDivyanshu <109081574+VanshDivyanshu@users.noreply.github.com>
2024-07-02 00:00:41 +0530

    Add files via upload

--- CWHL33.cpp
+++ CWHL33.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 
+class BankDeposit{
+    int principal;
+    int years;
+    float interestRate;
+    float returnValue;
+
+    public:
+        BankDeposit();
+        BankDeposit(int p, int y, float r);
+        BankDeposit(int p, int y, int r);
+        void show();
+};
+
+BankDeposit :: BankDeposit(int p, int y, lfoat r){
+    principal = p;
+    years = y;
+    interestRate = r;
+    returnValue = principal;
+    for (int i = 0; i < y; i++)
+    {
+        returnValue = returnValue * ( 1+ interestRate);
+    }
+}
+
+BankDeposit :: BankDeposit(int p, int y, int r){
+    principal = p;
+    years = y;
+    interestRate = float(r)/100;
+    returnValue = principal;
+    for (int i = 0; i < y; i++)
+    {
+        returnValue = returnValue * ( 1+ interestRate);
+    }
+}
+
+void BankDeposit :: show(){
+    cout<<endl<<"Principal amount was "<<principal<<". Return value after "<<years<<" years is "<<returnValue<<endl;
+}
+
 int main(){
-    cout<<"Hello world";
+    BankDeposit bd1,bd2,bd3;
     return 0;
 }
